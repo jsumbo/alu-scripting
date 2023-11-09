7705a18f5c6719ddc7c8536a2ddbf27a0bf58d57
Jallah Sumbo <j.smbo@alustudent.com>
2023-11-09 12:05:21 +0200

    fix

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,14 +2,14 @@
 """
 This module contains a function to get the number of subscribers of a subreddit.
 """
-
 import requests
 
 def get_number_of_subscribers(subreddit):
-    """ returns the number of subscribers """
+    """Returns the number of subscribers or 0 if the subreddit does not exist."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
         subscribers = response.json().get('data').get('subscribers')
         if subscribers > 0:
@@ -17,6 +17,7 @@ def get_number_of_subscribers(subreddit):
     return "0"
 
 
-subreddit_name = "learnpython"  
+# Test the function with a specific subreddit
+subreddit_name = "learnpython" 
 result = get_number_of_subscribers(subreddit_name)
 print(result)
