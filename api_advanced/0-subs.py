d984a9e4726aaad97aca3139e7ba356b3242ae11
Jallah Sumbo <j.smbo@alustudent.com>
2023-11-09 11:52:44 +0200

    fix output error

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -7,10 +7,11 @@ import requests
 
 def get_number_of_subscribers(subreddit):
     """ returns the number of subscribers """
-    url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        return response.json().get('data').get('subscribers')
-    return 0
+        subscribers = response.json().get('data').get('subscribers')
+        if subscribers > 0:
+            return "OK"
+    return "0"
